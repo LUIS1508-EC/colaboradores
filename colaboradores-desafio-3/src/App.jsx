e845a01838aeea78216083ec3e8585f445c04273
LUIS1508-EC <151102084+LUIS1508-EC@users.noreply.github.com>
2024-05-06 00:22:10 -0400

    ....

---
+++ colaboradores-desafio-3/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import { BaseColaboradores } from './assets/components/Colaboradores'
+import Listado from './assets/components/Listado'
+import Formulario from './assets/components/Formularios'
+import Buscador from './assets/components/Buscador'
+import "./App.css";
+import Alert from './assets/components/Alertas'
+
+
+function App() {
+  const [colaboradores, setColaboradores] = useState(BaseColaboradores);
+  const [buscar, setBuscar] = useState('');
+  const [alerta, setAlerta] = useState({ error: false, mensaje: "", color: "" });
+
+  return (
+
+    <>
+      <div className="container-fluid">
+        <div className="row titulo1">
+          <h1>Lista de Colaboradores</h1>
+        </div>
+        <div className="row" id="buscador">
+          <Buscador buscaColaborador={setBuscar} />
+        </div>
+        <div id="listado">
+          <Listado
+            colaboradores={colaboradores}
+            buscaColaborador={buscar}
+          />
+        </div>
+        <div className='titulo2'>
+          <h4 className="mb-2 pb-2">Agregar colaborador</h4>
+        </div>
+        <div id="formulario">
+          <Formulario
+            colaboradores={colaboradores}
+            setColaboradores={setColaboradores}
+            setAlerta = {setAlerta}
+            alerta = {alerta}
+          />
+        </div>
+        <div id="alerta">
+          {alerta.error ? (
+            <Alert mensaje={alerta.mensaje} color={alerta.color} />
+          ) : null}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
